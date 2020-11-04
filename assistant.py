a496e0f19f592b169cad92393210f386f1e1555e
Ben J <P2423891@my365.dmu.ac.uk>
2020-11-04 19:10:03 +0000

    Laying foundations for voting system

--- assistant.py
+++ assistant.py
@@ -9,8 +9,51 @@ import requests
 import os
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv, find_dotenv
+import datetime
+import time
+import pickle
+import os.path
 
 load_dotenv(find_dotenv())
+
+
+class ballot(dict):
+    def __init__(self,name):
+    	self.name = name
+    	if os.path.exists(f"{self.name}.pkl"):
+    		loaded_ballot_box = pickle.load(open(f"{self.name}.pkl", "rb" ))
+    		for key in loaded_ballot_box:
+    			self[key] = loaded_ballot_box[key]
+    	else:
+    		self.name = name
+    		self["1"] = {"voter_id":"1","voter_name":"1","nominee_id":"1","nominee_tag":"1","timestamp":str(time.time())}
+    		pickle.dump(self, open(f"{self.name}.pkl", "wb" ))
+    
+    def vote(self,voter_id,voter_tag,nominee_id,nominee_tag):
+        prev_hash = list(x.items())[-1][0]
+        prev_results = list(x.items())[-1][1]
+        for key in prev_results:
+            prev_hash += prev_results[key]
+            new_hash = hashlib.sha256(str.encode(prev_hash)).hexdigest()
+            self[new_hash] = {"voter_id":voter_id,
+            				  "voter_name":voter_tag,
+            				  "nominee_id":nominee_id,
+            				  "nominee_tag":nominee_tag,
+            				  "timestamp":str(time.time())}
+            print(new_hash, self[new_hash])
+    
+    def backup(self):
+    	if os.path.exists(f"{self.name}.pkl"):
+    		os.rename(f"{self.name}.pkl", f"{self.name} {datetime.datetime.now().strftime('%d-%m-%Y %H-%M-%S')}.pkl")
+    		pickle.dump(self, open(f"{self.name}.pkl", "wb" ))
+    	else:
+            pickle.dump(self, open(f"{self.name}.pkl", "wb" ))
+
+vote_helpful = ballot("helpful")
+vote_theme = ballot("theme")
+vote_funny = ballot("funny")
+vote_improved = ballot("improved")
+vote_best = ballot("best")
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
@@ -58,6 +101,26 @@ async def get_specs(session, url):
     return None
 
 @bot.command(pass_context=True)
+async def vote(ctx):
+    user = bot.get_user(ctx.message.author.id)
+    await ctx.send("<@!"+str(ctx.message.author.id)+"> you have been DM'd voting instructions. Make sure you are able to receive DMs from server members.")
+    if ctx.message.author.joined_at.timestamp() < 1606780800:
+    	await user.send("""Thank you for participating in the Droider Of The Year awards.
+Please enter the username AND tag for the person you would like to nominate.
+Eg, Username#0001""")
+    else:
+    	await user.send("Sorry but your account did not join early enough to participate.")
+
+@bot.command(pass_context=True)
+async def backupvotes(ctx):
+	vote_helpful.backup()
+	vote_theme.backup()
+	vote_funny.backup()
+	vote_improved.backup()
+	vote_best.backup()
+	await ctx.send("Ballot boxes backed up!")
+
+@bot.command(pass_context=True)
 async def specs(ctx,*,device):
     """
     Function called when the !specs command is run.
@@ -66,7 +129,7 @@ async def specs(ctx,*,device):
     await ctx.trigger_typing()
     url = get_specs_url(device)
     if not url:
-        await ctx.send(f"Couldn't find the specs of the given device.")
+        await ctx.send("Couldn't find the specs of the given device.")
         return
     async with aiohttp.ClientSession() as session:
         stuff = await get_specs(session, url)
@@ -84,7 +147,7 @@ async def specs(ctx,*,device):
             if ctx.message.channel.name != "botspam":
                 await ctx.send("Please use bot commands in the appropriate \#botspam channel unless relevant to the current discussion, abuse of the bot will result in a mute.")
         else:
-            await ctx.send(f"Couldn't find the specs of the given device.")
+            await ctx.send("Couldn't find the specs of the given device.")
 
 @bot.command(pass_context=True)
 async def linkme(ctx,*,appSearch):
