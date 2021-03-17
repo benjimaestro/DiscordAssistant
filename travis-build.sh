d5a1454a04f11869fe0e25cbc61a353b8479187e
Ben J <P2423891@my365.dmu.ac.uk>
2021-03-17 19:06:33 +0000

    Configs, again

--- travis-build.sh
+++ travis-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cat assistant.py
-docker build -t zoemaestra/discord-bot .
+docker build -t benjimaestro/discord-bot .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker-compose bundle --push-images
